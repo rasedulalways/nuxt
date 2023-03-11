c7f648222f06a3aa3de008cfff7d6b62ec12ada9
rasedul <rasedul.tatu@gmail.com>
2023-03-11 03:27:56 +0200

    Route Parameters nuxt3 The Net Ninja Course - 4

--- nuxt-app/pages/products/[id].vue
+++ nuxt-app/pages/products/[id].vue
@@ -1,5 +1,16 @@
 <template>
-    <div class="container mx-auto">
-        
+    <div>
+        <!-- <p> Product details for id: {{ $route.params.id }} </p> -->
+        <p> Product details for {{ id }}</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo officiis eos deleniti, ex omnis exercitationem inventore, possimus dolore vel laborum, reiciendis facere cum quibusdam eaque? Fugiat aliquam perspiciatis exercitationem aut.</p>
     </div>
 </template>
+
+<script setup>
+    const{ id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
